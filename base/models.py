b8a766a6018805e4c52047b548271e7134c5a617
uchaen <u_chaen@naver.com>
2022-02-13 14:29:21 +0900

    third commit

--- base/models.py
+++ base/models.py
@@ -7,7 +7,6 @@ class Task(models.Model):
     description = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
     create = models.DateTimeField(auto_now_add=True)
-    #group = models.ForeignKey(Group, on_delete=models.CASCADE, null=True, blank=True)
 
     def __str__(self):
         return self.title
@@ -15,14 +14,3 @@ class Task(models.Model):
     class Meta:
         ordering = ['complete']
 
-
-class Group(models.Model):
-    name = models.CharField(max_length=128)
-    members = models.ManyToManyField(User, through='Membership')
-
-    def __str__(self):
-        return self.name
-
-class Membership(models.Model):
-    person = models.ForeignKey(User, on_delete=models.CASCADE)
-    group = models.ForeignKey(Group, on_delete=models.CASCADE)
